12f6800ea48653f4ee3ef071677f8055b58f28ef
momu54 <96234201+momu54@users.noreply.github.com>
2025-06-28 19:12:19 +0800

    fix: Fix filename

--- ResetEnvironment.py
+++ ResetEnvironment.py
@@ -1,10 +1,10 @@
 # Read in the file
-with open('file.txt', 'r') as file:
+with open('settings.toml', 'r') as file:
   filedata = file.read()
 
 # Replace the target string
 filedata = filedata.replace('production', 'development')
 
 # Write the file out again
-with open('file.txt', 'w') as file:
+with open('settings.toml', 'w') as file:
   file.write(filedata)
